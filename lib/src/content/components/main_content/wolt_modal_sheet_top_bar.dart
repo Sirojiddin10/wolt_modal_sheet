188d59bfcd7ad8aad1e9e95d03cbd925698bbd28
sirojiddin <s.odamboev@uic.group>
2024-11-26 11:53:21 +0500

    fix: remove colors from top bar

--- lib/src/content/components/main_content/wolt_modal_sheet_top_bar.dart
+++ lib/src/content/components/main_content/wolt_modal_sheet_top_bar.dart
@@ -27,21 +27,13 @@ class WoltModalSheetTopBar extends StatelessWidget {
     final defaultThemeData = WoltModalSheetDefaultThemeData(context);
 
     // Determine the top bar's elevation, shadow color, height, background color, and surface tint color.
-    final elevation =
-        themeData?.topBarElevation ?? defaultThemeData.topBarElevation;
-    final shadowColor = themeData?.topBarShadowColor ??
-        (theme.brightness == Brightness.light
-            ? const Color(0xFFE4E4E4)
-            : const Color(0xFF121212));
+    final elevation = 0.0;
+    final shadowColor = Colors.transparent;
     final topBarHeight = page.navBarHeight ??
         themeData?.navBarHeight ??
         defaultThemeData.navBarHeight;
-    final backgroundColor = page.backgroundColor ??
-        themeData?.backgroundColor ??
-        defaultThemeData.backgroundColor;
-    final surfaceTintColor = page.surfaceTintColor ??
-        themeData?.surfaceTintColor ??
-        defaultThemeData.surfaceTintColor;
+    final backgroundColor = Colors.transparent;
+    final surfaceTintColor = Colors.transparent;
 
     // Create the default top bar widget.
     SizedBox topBarWidget = SizedBox(
